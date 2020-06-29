6eb4f0f359f0575cca5a7c43739e02a031a4fc8f
smc181002 <meherchaitanya18802@gmail.com>
2020-06-29 13:52:50 -0400

    added sys.argv option

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from random import randint
+import sys
 
-accuracy = randint(70,90)
+try:
+  hyperParams = int(sys.argv[1])
+except:
+  print('enter a number and not any other datatype.')
+
+
+accuracy = randint(70, hyperParams)
 with open('command.txt', 'w') as file:
     file.write(str(accuracy))
-print(accuracy)
+print(accuracy, hyperParams)
